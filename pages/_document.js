01b467a8c60745dd3d54d1e40cb1502061eca3b3
Rasmus Andersen <rasmus@growupstudio.dk>
2022-05-24 16:26:56 +0200

    mailchimp tag

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html lang="da">
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,500;1,500&family=Nunito+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet" />
-          <script id="mailChimp_script" dangerouslySetInnerHTML={{
+          <script id="mcjs" dangerouslySetInnerHTML={{
               __html: `
               !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/afa7bafb8da9deb3bd242c3f0/fcfc769ee2bf84697a81293cb.js");
               `}}
